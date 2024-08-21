d2381bc75a9f1ef0141d643cb880bf926123a0f3
TaeYoung1217 <wkdxodud44@gmail.com>
2024-08-21 15:25:01 +0900

    SQL 수정

--- table_ddl.sql
+++ table_ddl.sql
@@ -1,7 +1,5 @@
 CREATE DATABASE `superCodingProject1st` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
-CREATE DATABASE `superCodingProject1st` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-
 CREATE TABLE `comments` (
                             `id` int NOT NULL AUTO_INCREMENT,
                             `post_id` int NOT NULL,
@@ -9,16 +7,9 @@ CREATE TABLE `comments` (
                             `content` text NOT NULL,
                             `created_at` datetime NOT NULL,
                             PRIMARY KEY (`id`),
-                            KEY `comments_posts_FK` (`post_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `great` (
-                         `id` int NOT NULL AUTO_INCREMENT,
-                         `post_id` int NOT NULL,
-                         PRIMARY KEY (`id`),
-                         KEY `great_posts_FK` (`post_id`),
-                         CONSTRAINT `great_posts_FK` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+                            KEY `comments_posts_FK` (`post_id`),
+                            CONSTRAINT `comments_posts_FK` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `posts` (
                          `id` int NOT NULL AUTO_INCREMENT,
@@ -26,17 +17,11 @@ CREATE TABLE `posts` (
                          `content` longtext NOT NULL,
                          `created_at` datetime NOT NULL,
                          `user_id` int NOT NULL,
-                         `comments_id` int DEFAULT NULL,
-<<<<<<< HEAD
-=======
-                         -- like 갯수를 저장할  column 추가
-                         `like_count` int DEFAULT 0,
-
->>>>>>> develop_great
+                         `like_count` int DEFAULT NULL,
                          PRIMARY KEY (`id`),
-                         KEY `posts_users_FK` (`user_id`),
-                         CONSTRAINT `posts_users_FK` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+                         KEY `user_id` (`user_id`),
+                         CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `roles` (
                          `id` int NOT NULL AUTO_INCREMENT,
@@ -49,7 +34,19 @@ CREATE TABLE `tokenStatus` (
                                `token` text NOT NULL,
                                `isValid` tinyint(1) NOT NULL,
                                PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=67 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `userLikes` (
+                             `id` int NOT NULL AUTO_INCREMENT,
+                             `user_id` int DEFAULT NULL,
+                             `post_id` int DEFAULT NULL,
+                             PRIMARY KEY (`id`),
+                             UNIQUE KEY `id` (`id`),
+                             KEY `user_id` (`user_id`),
+                             KEY `post_id` (`post_id`),
+                             CONSTRAINT `userlikes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+                             CONSTRAINT `userlikes_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=109 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `userPrincipalRoles` (
                                       `id` int NOT NULL AUTO_INCREMENT,
@@ -60,7 +57,7 @@ CREATE TABLE `userPrincipalRoles` (
                                       KEY `userPrincipalRoles_roles_FK` (`role_id`),
                                       CONSTRAINT `userPrincipalRoles_roles_FK` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`),
                                       CONSTRAINT `userPrincipalRoles_users_FK` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `users` (
                          `id` int NOT NULL AUTO_INCREMENT,
@@ -70,4 +67,4 @@ CREATE TABLE `users` (
                          `created_at` datetime NOT NULL,
                          PRIMARY KEY (`id`),
                          UNIQUE KEY `users_unique` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
